ad989a6e0fc20a470eda0d6fbe180660acdf9b89
Anurag Meena <anuragmeena92@gmail.com>
2016-02-13 20:08:36 +0530

    minor

--- app/models.py
+++ app/models.py
@@ -13,3 +13,5 @@ class Contact(BaseModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.contact
